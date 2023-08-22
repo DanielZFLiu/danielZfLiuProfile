d974714954b734e077e470f170ed897682d071f2
DanielZFLiu <daniel.zf.liu@gmail.com>
2023-08-22 12:51:59 -0400

    basic setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss'],
+  app: {
+    head: {
+      title: 'Library of Vita',
+      meta: [
+        {name: 'description', content: 'Personal Profile of Daniel Liu '}
+      ],
+    },
+  },
 })
